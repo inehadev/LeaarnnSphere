aa04bf373f0c18e171f044e1731488253fd00fc1
inehadev <n988420@gmail.com>
2024-10-24 22:32:47 +0530

    update with lopgin and integration of api

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Router  , Routes , Route} from 'react-router-dom'
 import Hero from './pages/Hero'
 import Dashboard from './pages/Dashboard'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
    
     <Routes>
       <Route path = '/' element= {<Hero/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
     </Routes>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import axios from "axios";
 
- export default function SignUp () {
+ export default function SignIn () {
+
     const [email , setemail]=useState('');
-    const [username , setusername]=useState('')
     const[password , setpassword]=useState('')
 
     const handleResponse = async(e)=>{
         e.preventDefault();
         
         const bodyParmeter = {
-            username : username,
+            
             email: email,
             password : password
 
@@ -24,22 +25,18 @@ import axios from "axios";
             }
         }
 
-        const response = await axios.post("http://localhost:3000/register" ,  bodyParmeter , axiosheader );
+        const response = await axios.post("http://localhost:3000/login" ,  bodyParmeter , axiosheader );
         console.log(response);
 
-        alert("user is register successfully")
+        alert("user is login successfully")
     }
 
     return(
         <>
        <div className="h-screen w-full  flex justify-center items-center" >
-        <div className=" h-[400px] w-[420px] mb-[5%]    text-opacity-75 rounded-lg border  border-blue-200 border-opacity-30" >
-            <h2 className="text-2xl text-center mt-5 text-white tracking-wide">SignUp</h2>
-            <div className=" w-full mx-5 mt-7">
-                <p className="text-lg text-opacity-75 text-blue-200">username</p>
-                <input className=" h-9 w-[90%]  text-white text-opacity-60  rounded-md border border-blue-700 border-opacity-40  bg-transparent  " type="text" value={username} onChange={(e)=>{setusername(e.target.value)}} />
-            </div>
-
+        <div className=" h-[350px] w-[420px] mb-[5%]    text-opacity-75 rounded-lg border  border-blue-200 border-opacity-30" >
+            <h2 className="text-2xl text-center mt-5 text-white tracking-wide">SignIn</h2>
+          
             <div className=" w-full mx-5 mt-3">
                 <p className="text-lg text-opacity-75 text-blue-200">email </p>
                 <input className=" h-9 w-[90%]  rounded-md text-white text-opacity-60  border border-blue-700 border-opacity-40  bg-transparent  " type="email" value={email} onChange={(e)=>{setemail(e.target.value)}} />
@@ -52,9 +49,8 @@ import axios from "axios";
 
             <div className=" w-full mx-5 mt-5">
 
-                <button className=" h-9 w-[90%]  rounded-full bg-blue-700 bg-opacity-70 text-white border border-blue-700 border-opacity-40    " onClick={handleResponse}>Register</button>
-                <p className="text-white mt-3 text-opacity-70 text-center mr-7"> Already have an account? <Link to="/sign-in"><span className="text-blue-500">SignIn</span></Link></p>
-
+                <button className=" h-9 w-[90%]  rounded-full bg-blue-700 bg-opacity-70 text-white border border-blue-700 border-opacity-40    " onClick={handleResponse}>Login</button>
+                <p className="text-white mt-3 text-opacity-70 text-center mr-7">Don't have an account? <Link to="/sign-up"><span className="text-blue-500">SignUp</span></Link></p>
               
             </div>
 
